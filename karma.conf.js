d0d65183a5a5d216f71cf8da56a7fbb11818693c
brice.matias.e <brice.matias.e@thalesdigital.io>
2024-04-30 11:52:28 +0200

    Init project ci

--- karma.conf.js
+++ karma.conf.js
@@ -1,29 +1,33 @@
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-
+    basePath: "",
+    frameworks: ["jasmine", "@angular-devkit/build-angular"],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require("karma-jasmine"),
+      require("karma-chrome-launcher"),
+      require("karma-jasmine-html-reporter"),
+      require("karma-coverage"),
+      require("@angular-devkit/build-angular/plugins/karma"),
     ],
-
-    browsers: ['ChromeHeadless'], // Utilisation de Chrome en mode headless pour les tests.
-
-    reporters: ['progress', 'kjhtml', 'coverage'],
-    coverageReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
-      subdir: '.',
-      reporters: [
-        { type: 'lcovonly', subdir: '.' },
-        { type: 'text-summary' }
-      ]
+    client: {
+      jasmine: {
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    singleRun: false,
-    restartOnFileChange: true
+    jasmineHtmlReporter: {
+      suppressAll: true, // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require("path").join(__dirname, "./coverage/angular-17-crud"),
+      subdir: ".",
+      reporters: [{ type: "lcov" }],
+    },
+    reporters: ["progress", "kjhtml"],
+    browsers: ["Chrome"],
+    restartOnFileChange: true,
   });
 };
