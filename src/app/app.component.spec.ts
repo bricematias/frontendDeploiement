7aee5b286fd00749c74eb7730ea658f44f340c86
brice.matias.e <brice.matias.e@thalesdigital.io>
2024-04-30 09:40:46 +0200

    Init project ci

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientTestingModule
       ],
       declarations: [
         AppComponent
@@ -23,7 +25,7 @@ describe('AppComponent', () => {
   it(`should have as title 'angular-17-crud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-17-crud');
+    expect(app.title).toEqual('Angular 17 Crud example');
   });
 
   it('should render title', () => {
